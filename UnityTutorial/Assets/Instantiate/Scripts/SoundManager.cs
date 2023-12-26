e675d05b69377b4cc1664a47409799df54fd735e
jjunyoung97 <106666357+jjunyoung97@users.noreply.github.com>
2023-12-26 19:45:26 +0900

    20231226

---
+++ UnityTutorial/Assets/Instantiate/Scripts/SoundManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+
+public class Sound
+{
+    public AudioClip[] audioClips; 
+}
+
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField] AudioSource bgmSource;
+    [SerializeField] AudioSource sfxSource;
+
+    public static SoundManager instance = null;
+
+    void Awake()
+    {
+        if(instance == null)
+        {
+            instance = null;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void Sound(AudioClip audioClip)
+    {
+        // PlayOneShot : 동시에 여러 위치에서 사운드를 호출하는 함수
+        sfxSource.PlayOneShot(audioClip);
+    }
+}
--- UnityTutorial/Assets/Instantiate/Scripts/Unit/Unit.cs
+++ UnityTutorial/Assets/Instantiate/Scripts/Unit/Unit.cs
@@ -2,8 +2,122 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class Unit : MonoBehaviour // abstract 추상클래스
+public enum State
 {
-    public abstract void Move();
+    Move,
+    Attack,
+    Die,
+    None
+}
 
+public abstract class Unit : MonoBehaviour
+{
+    [SerializeField] GameObject target;
+
+    [SerializeField] State state;
+
+    [SerializeField] Animator animator;
+    [SerializeField] Vector3 direction;
+    [SerializeField] Vector3 targetDirection;
+
+    [SerializeField] float speed = 5.0f;
+    [SerializeField] protected float health;
+
+    [SerializeField] Sound sound = new Sound();
+
+    public void OnHit(float damage)
+    {
+        health -= damage;
+
+        if (health <= 0)
+        {
+            state = State.Die;
+        }
+    }
+
+    public virtual void Release()
+    {
+        Destroy(gameObject);
+    }
+
+    private void Awake()
+    {
+        target = GameObject.Find("Player");
+        animator = GetComponent<Animator>();
+    }
+
+    public void Update()
+    {
+        switch (state)
+        {
+            case State.Move:
+                Move();
+                break;
+            case State.Attack:
+                Attack();
+                break;
+            case State.Die:
+                Die();
+                break;
+        }
+    }
+
+    public virtual void Move()
+    {
+        animator.SetBool("Attack", false);
+
+        // 1. Target의 벡터 - 자신의 위치 벡터
+        direction = target.transform.position - transform.position;
+        targetDirection = target.transform.position;
+
+        // 2. y축을 0으로 설정합니다.
+        direction.y = 0;
+        targetDirection.y = 0;
+
+        // 3. 벡터의 정규화
+        direction.Normalize();
+
+        // 4. 특정한 대상을 바라봅니다.
+        transform.LookAt(targetDirection);
+
+        // 5. 방향 벡터를 구한 값을 가지고 이동을 합니다.
+        transform.position += direction * speed * Time.deltaTime;
+    }
+
+    public virtual void Attack()
+    {
+        
+        animator.SetBool("Attack", true);
+    }
+
+    public void AttackSound()
+    {
+        SoundManager.instance.Sound(sound.audioClips[0]);
+    }
+
+    public virtual void Die()
+    {
+        animator.Play("Die");
+        SoundManager.instance.Sound(sound.audioClips[1]);
+
+        state = State.None;
+    }
+
+    // OnTriggerEnter() : Trigger 충돌이 되었을 때 이벤트를 호출하는 함수입니다.
+    private void OnTriggerEnter(Collider other)
+    {
+        state = State.Attack;
+    }
+
+    // OnTriggerStay() : Trigger가 충돌 중일 때 이벤트를 호출하는 함수입니다.
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log("OnTriggerStay");
+    }
+
+    // OnTriggerExit() : Trigger와 충돌이 끝났을 때 이벤트를 호출하는 함수입니다.
+    private void OnTriggerExit(Collider other)
+    {
+        state = State.Move;
+    }
 }
